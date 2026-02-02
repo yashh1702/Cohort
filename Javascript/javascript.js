4eb159d63aba8cfcce71d313343b48977d6c9704
yashh1702 <yagrawal944@gmail.com>
2026-02-02 21:33:10 +0530

    drag and drop

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -98,20 +98,75 @@
 // })
 
 
-function instagramSeDataLaao(username,cb){
-    setTimeout(() => {
-       cb({uid:11,username:username}) 
-    }, 3000);
-}
+// function instagramSeDataLaao(username,cb){
+//     setTimeout(() => {
+//        cb({uid:11,username:username}) 
+//     }, 3000);
+// }
 
-function metaPeJaaoDataLaao(uid,cb){
-    setTimeout(() => {
-        cb(["img1","img2"])
-    }, 4000);
-}
+// function metaPeJaaoDataLaao(uid,cb){
+//     setTimeout(() => {
+//         cb(["img1","img2"])
+//     }, 4000);
+// }
 
-instagramSeDataLaao("yash",function(data){
-    metaPeJaaoDataLaao(data.uid,function(images){
-        console.log(data.uid,images)
-    })
-})
+// instagramSeDataLaao("yash",function(data){
+//     metaPeJaaoDataLaao(data.uid,function(images){
+//         console.log(data.uid,images)
+//     })
+// })\
+
+// document.addEventListener("mousedown", function (e) {
+//     if (e.button === 2) {
+//         console.log("Right click");
+//     }
+// });
+
+const box = document.getElementById("box");
+
+let isDragging = false;
+let offsetX, offsetY;
+
+box.addEventListener("mousedown", (e) => {
+  isDragging = true;
+
+  // mouse aur box ke beech ka gap
+  offsetX = e.clientX - box.offsetLeft;
+  offsetY = e.clientY - box.offsetTop;
+
+  box.style.cursor = "grabbing";
+});
+
+// document.addEventListener("mousemove", (e) => {
+//   if (!isDragging) return;
+
+//   box.style.left = e.clientX - offsetX + "px";
+//   box.style.top  = e.clientY - offsetY + "px";
+// });
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+
+  let x = e.clientX - offsetX;
+  let y = e.clientY - offsetY;
+
+  // screen / body size
+  const maxX = window.innerWidth - box.offsetWidth;
+  const maxY = window.innerHeight - box.offsetHeight;
+
+  // boundaries apply
+  if (x < 0) x = 0;
+  if (y < 0) y = 0;
+  if (x > maxX) x = maxX;
+  if (y > maxY) y = maxY;
+
+  box.style.left = x + "px";
+  box.style.top  = y + "px";
+});
+
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  box.style.cursor = "grab";
+});
+
