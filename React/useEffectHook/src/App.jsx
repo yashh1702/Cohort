d641a39a5295a07d41b840915f753c0e0b01fda2
yashh1702 <yagrawal944@gmail.com>
2026-02-09 19:39:11 +0530

    implemented axios and create random name

--- React/useEffectHook/src/App.jsx
+++ React/useEffectHook/src/App.jsx
@@ -1,28 +1,29 @@
 import  { useEffect, useState } from 'react'
-
+import axios from 'axios'
 const App = () => {
 
   const [number1, setNumber1] = useState(0)
-  const [number2, setNumber2] = useState(0)
+  const [username, setUsername] = useState("")
+
+  const getData = async () => {
+    const response = await axios.get("https://randomuser.me/api/")
+    setUsername((response.data.results[0].name.first) + " " + (response.data.results[0].name.last) )
+  }
 
   useEffect(function(){
-    console.log("useEffect is Running")
-  },[number1,setNumber2])
+    getData()
+  },[number1])
   
 
 
   return (
     <div>
-      <h1>{number1}</h1>
+      <h1>{username}</h1>
+      <h2>{number1}</h2>
       <button onClick={() => {
-        setNumber1(Math.floor(Math.random() * 100))
-      }}>Change Number 1</button>
-      <br/> <br/>
-
-      <h1>{number2}</h1>
-      <button onClick={() => {
-        setNumber2(Math.floor(Math.random() * 100))
-      }}>Change Number 2</button>
+        setNumber1(number1+1)
+      }}>Click here</button>
+      
     </div>
   )
 }
