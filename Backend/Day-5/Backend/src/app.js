0317aac1bd6816407cc485d120a86f9686fd5a65
yashh1702 <yagrawal944@gmail.com>
2026-02-14 02:28:37 +0530

    connected axios and cors to get the data from backend

--- Backend/Day-5/Backend/src/app.js
+++ Backend/Day-5/Backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const noteModel = require("./models/note.model.js")
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 // Creating the note
--- Backend/Day-5/Frontend/src/App.jsx
+++ Backend/Day-5/Frontend/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [notes, setNotes] = useState([
+  ])
+
+  axios.get("http://localhost:3000/api/notes")
+  .then((res)=>{
+    setNotes(res.data.notes)
+  })
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='font-bold p-10 gap-5 bg-black text-white flex '>
+     {notes.map(note => {
+      return <div className="p-5 border rounded-2xl max-w-3/12 flex flex-col  items-center gap-2">
+        <h1 className="font-extraboldbold text-4xl ">{note.title}</h1>
+        <p className="">{note.description}</p>
+        <div className="flex justify-between gap-10 mt-2 text-sm">
+          <button className=" px-3 py-1 cursor-pointer active:scale-95 bg-rose-600 rounded-2xl">Remove</button>
+          <button className=" px-3 py-1 cursor-pointer active:scale-95 bg-green-600 rounded-2xl">Update</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     })}
+      
+    </div>
   )
 }
 
